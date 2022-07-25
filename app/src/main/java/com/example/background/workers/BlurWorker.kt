34d8fccc20f8da88069ad522e7deafe2d044ea91
NovalAdi <novaladiperasetya@gmail.com>
2022-07-25 09:39:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,37 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.R
+
+private const val TAG = "BlurWorker"
+class BlurWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        val appContext = applicationContext
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
+        return try {
+            val picture = BitmapFactory.decodeResource(
+                appContext.resources,
+                R.drawable.android_cupcake
+            )
+
+            val output = blurBitmap(picture, appContext)
+
+            // Write bitmap to a temp file
+            val outputUri = writeBitmapToFile(appContext, output)
+
+            makeStatusNotification("Output is $outputUri", appContext)
+
+            Result.success()
+        } catch (throwable: Throwable) {
+            Log.e(TAG, "Error applying blur")
+            Result.failure()
+        }
+    }
+}
